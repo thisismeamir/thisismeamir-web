3dcf876b4614c273a8ab41379ae38d5f20efc7f8
Kid A <thisismeamir@outlook.com>
2024-10-19 01:23:56 +0330

    [Publishment]

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import "./assets/styling/main.css"
-import { createApp } from 'vue'
-import App from './App.vue'
-import {router} from "@/routes/router.ts";
-
-createApp(App)
-    .use(router)
-    .mount('#app')
-
